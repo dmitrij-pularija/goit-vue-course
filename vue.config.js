47eea0aef3058758bdcddc7d351f6bf2e6d284dc
dmitrij-pularija <dmitrij.pularija@gmail.com>
2023-05-10 18:21:15 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,6 @@ const { defineConfig } = require('@vue/cli-service')
 // })
 
 module.exports = defineConfig({
-  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
   ? '/goit-vue-course/'
   : '/'
